231f07764e1ef1e6512483e2496cfd4a841dbc3b
pablo.ordunya <devnull@localhost>
2012-06-06 18:16:10 +0200

    Update Moodle 1.9 plug-in

---
+++ lms/moodle/1.9/lms4labs/lms/authenticate.php
@@ -0,0 +1,54 @@
+<?php
+
+// This file is part of lms4labs
+//
+// lms4labs is free software: you can redistribute it and/or modify
+// it under the terms of the BSD 2-Clause License
+
+// lms4labs is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//
+
+/**
+ * This script redirects LMS administrator to the Lab Manager acting as a 
+ * manager of this LMS.
+ *
+ * @copyright 2012 Elio San Cristobal, Alberto Pesquera Martin, Pablo Orduña
+ * @license http://opensource.org/licenses/BSD-2-Clause BSD 2-Clause License
+ * @package lms4labs
+ */
+
+
+require_once("../../../config.php");
+require_once("lib/HttpClient.class.php");
+require_once("../../../lib/accesslib.php");
+
+global $CFG, $USER;
+
+
+$auxname=$USER->firstname. " " . $USER->lastname ;
+
+
+if (is_siteadmin($USER->id)) {
+      $request_data = array( 
+                	"full-name"  => $auxname,
+      ); 
+	$request= json_encode($request_data);
+	$client = new HttpClient('weblab.ieec.uned.es:5000');
+	$client->setAuthorization("uned", "password");
+	$client->post('/lms4labs/labmanager/lms/admin/authenticate/',$request);
+	$content = $client->getContent(); 
+	echo $content; 	
+}
+else
+ {
+	echo "error:You don't have permissions";
+}
+
+
+
+
+
+
+ 
